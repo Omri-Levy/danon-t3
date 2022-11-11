f27bc9de9758fcb88acbd54008ef3aa018e4e911
Omri Levy <omri.levy0197@gmail.com>
2022-11-11 12:49:59 +0200

    fix(isDuplicateEntryError): refactored to work with planetscale in production
    in production the database driver returns a different error object than in dev

--- src/common/utils/is-duplicate-entry-error/is-duplicate-entry-error.ts
+++ src/common/utils/is-duplicate-entry-error/is-duplicate-entry-error.ts
@@ -1,14 +1,36 @@
+import { env } from '../../env/server.mjs';
 import { IDuplicateEntryError } from './interfaces';
+import { isObject } from '../is-object/is-object';
 
 export const isDuplicateEntryError = (
 	error: unknown,
 ): error is IDuplicateEntryError => {
+	if (env.NODE_ENV !== 'production') {
+		return (
+			isObject(error) &&
+			'code' in error &&
+			(error as Record<string, unknown>).code ===
+				'ER_DUP_ENTRY' &&
+			typeof (error as Record<string, unknown>).sqlMessage ===
+				'string'
+		);
+	}
+
 	return (
-		typeof error === 'object' &&
-		error !== null &&
-		'code' in error &&
-		(error as Record<string, unknown>).code === 'ER_DUP_ENTRY' &&
-		typeof (error as Record<string, unknown>).sqlMessage ===
-			'string'
+		isObject(error) &&
+		'body' in error &&
+		'message' in error.body &&
+		(
+			error as {
+				body: {
+					message: string;
+				};
+			}
+		).body.message.includes('code = AlreadyExists') &&
+		typeof (
+			error as {
+				body: Record<PropertyKey, unknown>;
+			}
+		).body.message === 'string'
 	);
 };
